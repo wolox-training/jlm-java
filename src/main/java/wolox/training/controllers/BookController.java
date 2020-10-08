152f66465b124dd6b0ae00445e9cf5bd2ffacb15
JAIME LUIS MORALES GUZMAN <jaimeluismoralesguzman@MacBook-Pro-de-WOLOX.local>
2020-10-08 11:14:31 -0500

    Book controller added and its view

---
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -0,0 +1,22 @@
+package wolox.training.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BookController {
+
+    @GetMapping("/greeting")
+    public String greeting(
+        @RequestParam(name = "name", required = false, defaultValue = "Mundo") String name,
+        Model model) {
+
+        model.addAttribute("name", name);
+
+        return "greeting";
+
+    }
+
+}
