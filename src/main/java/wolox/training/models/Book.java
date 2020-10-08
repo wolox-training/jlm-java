c83495546c0eef4e24b00cf16a96af96885468f6
JAIME LUIS MORALES GUZMAN <jaimeluismoralesguzman@MacBook-Pro-de-WOLOX.local>
2020-10-08 14:28:56 -0500

    Book model added, lomok dependency added

---
+++ src/main/java/wolox/training/models/Book.java
@@ -0,0 +1,47 @@
+package wolox.training.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    private long id;
+
+    private String genre;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, name = "sub_title")
+    private String subTitle;
+
+    @Column(nullable = false)
+    private String publisher;
+
+    @Column(nullable = false)
+    private String year;
+
+    @Column(nullable = false)
+    private int pages;
+
+    @Column(nullable = false)
+    private String isbn;
+
+}
