f4c92a78b3fa099a0c7afdb23ab3c4da87bd33cb
JAIME LUIS MORALES GUZMAN <jaimeluismoralesguzman@MacBook-Pro-de-WOLOX.local>
2020-10-09 19:50:19 -0500

    Javadoc added

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -27,6 +27,10 @@ public class BookController {
 
     }
 
+    /**
+     * Method that gets all books
+     * @return all {@link Book}
+     */
     @GetMapping()
     public Iterable<Book> findAll() {
 
@@ -34,6 +38,11 @@ public class BookController {
 
     }
 
+    /**
+     * Method that obtain a book by id
+     * @param id: Book identifier (Long)
+     * @return {@link Book}
+     */
     @GetMapping("/{id}")
     public Book findById(@PathVariable Long id) {
 
@@ -42,6 +51,11 @@ public class BookController {
 
     }
 
+    /**
+     * Book saving method
+     * @param book: Request body ({@link Book})
+     * @return {@link Book}
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book save(@RequestBody Book book) {
@@ -50,6 +64,12 @@ public class BookController {
 
     }
 
+    /**
+     * Book updating method
+     * @param book: Request body ({@link Book})
+     * @param id: Book identifier (Long)
+     * @return {@link Book}
+     */
     @PutMapping("/{id}")
     public Book update(@RequestBody Book book, @PathVariable Long id) {
 
@@ -65,6 +85,10 @@ public class BookController {
 
     }
 
+    /**
+     * Book deleting method
+     * @param id: Book identifier (Long)
+     */
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
 
