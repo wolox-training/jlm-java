baa240f5b12d0967eb5abe2af2ac40c15d3d87f2
JAIME LUIS MORALES GUZMAN <jaimeluismoralesguzman@MacBook-Pro-de-WOLOX.local>
2020-10-13 08:26:39 -0500

    Add javadoc in GreetingController

--- src/main/java/wolox/training/controllers/GreetingController.java
+++ src/main/java/wolox/training/controllers/GreetingController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
+    /**
+     * Method that gets a greeting by name
+     * @param name: Name to greet (String)
+     * @param model: Object to model the view ({@link Model})
+     * @return greet (String)
+     */
     @GetMapping("/greeting")
     public String greeting(
         @RequestParam(name = "name", required = false, defaultValue = "World") String name,
