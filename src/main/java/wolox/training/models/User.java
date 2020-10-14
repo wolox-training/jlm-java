de9977ec9c93d1a08b1ab7da135e70da38ab50b3
JAIME LUIS MORALES GUZMAN <jaimeluismoralesguzman@MacBook-Pro-de-WOLOX.local>
2020-10-13 23:20:16 -0500

    Change in relation manyToMany

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -3,10 +3,13 @@ package wolox.training.models;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
@@ -42,7 +45,11 @@ public class User {
     private LocalDate birthdate;
 
     @NotNull
-    @ManyToMany(mappedBy = "users")
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "users_book",
+        joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id")
+    )
     private List<Book> books = Collections.emptyList();
 
     public List<Book> getBooks() {
